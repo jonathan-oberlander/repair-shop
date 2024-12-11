70d5cfc82fa6ee47c70b9c7284e5473535c772a4
jonathan-oberlander <jonathan.oberlander@gmail.com>
2024-12-11 13:53:47 +0000

    setup neon with drizzle

---
+++ db/migrate.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { db } from "./index";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+
+async function main() {
+  try {
+    await migrate(db, {
+      migrationsFolder: "./db/migrations",
+    });
+
+    console.log("Migration completed");
+  } catch (error) {
+    console.error("Error during migration: ", error);
+    process.exit(1);
+  }
+}
+
+main();
